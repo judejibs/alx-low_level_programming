b6bd22eb4297ecfd71d28cda08e271b76bc08f41
judejibs <judejibs@gmail.com>
2022-12-11 13:45:34 -0800

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,15 +8,18 @@
 int main(void)
 {
 	char c;
+
 	char d;
+
 	c = 'a';
 	d = 'A';
-	while 
+	while
 		(c <= 'z') {
 			putchar(c);
 			c++;
 		}
-	while (d <= 'Z') {
+	while (d <= 'Z')
+	{
 		putchar(d);
 		d++;
 	}
